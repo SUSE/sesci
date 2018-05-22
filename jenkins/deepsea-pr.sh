23968e18a8114abb0f4f4da617fcaf0acbd98111
Kyrylo Shatskyy <kyrylo.shatskyy@gmail.com>
2018-05-22 11:43:56 +0200

    Cleanup deepsea pr related code

--- jenkins/deepsea-pr.sh
+++ jenkins/deepsea-pr.sh
@@ -5,15 +5,18 @@ mkdir -p logs
 # Job specific variables
 TEUTH_NAME=${TEUTH_NAME:-"ci"}
 TEUTH_BRANCH=${TEUTH_BRANCH:-"master"}
-PRTGT_BRANCH=${ghprbTargetBranch:-"ses5"}
 
-if [[ "$PRTGT_BRANCH" == "master" ]] ; then
-    CEPH_BRANCH="ses6"
-    SUITE_BRANCH="ses6"
-else
-    CEPH_BRANCH="${PRTGT_BRANCH,,}"
-    SUITE_BRANCH="${PRTGT_BRANCH,,}"
-fi
+CEPH_BRANCH=${CEPH_BRANCH:-"ses6"}
+SUITE_BRANCH=${SUITE_BRANCH:-"ses6"}
+
+#PRTGT_BRANCH=${ghprbTargetBranch:-"ses5"}
+#if [[ "$PRTGT_BRANCH" == "master" ]] ; then
+#    CEPH_BRANCH="ses6"
+#    SUITE_BRANCH="ses6"
+#else
+#    CEPH_BRANCH="${PRTGT_BRANCH,,}"
+#    SUITE_BRANCH="${PRTGT_BRANCH,,}"
+#fi
 
 
 TEUTH_PATH=${TEUH_PATH:-"$HOME/teuthology"}
@@ -31,49 +34,6 @@ ART_PATH=${ART_PATH:-"deepsea/PR/${JOB_NAME/-trigger/}-${BUILD_ID}"}
 PULL_ID=${ghprbPullId:-"0"}
 
 TEUTH_LOG=logs/teuth-deepsea-pr$PULL_ID-b$BUILD_ID.log
-
-# Next parameters supposed to be always declared by the Jenkins job.
-# The PUBLISH_DIR is used by rpmbuild to copy built rpms for later
-# repository creation. The ACCESS_URL is used by teuthology to add
-# is a test repo with deepsea artifacts, which will override the once
-# in SCC because they have always newer version.
-#
-# Value examples below:
-#
-# PUBLISH_DIR   /mnt/logs/artifacts/jenkins/$ART_PATH
-# ACCESS_URL    http://storage-ci.suse.de/artifacts/jenkins/$ART_PATH
-
-PUBLISH_DIR=${PUBLISH_DIR:-"/mnt/logs/artifacts/jenkins/$ART_PATH"}
-ACCESS_URL=${ACCESS_URL:-"http://storage-ci.suse.de/artifacts/jenkins/$ART_PATH"}
-
-# ------------------------------------------------------------------------------
-# BUILD RPMS
-
-# #RPMBUILD=${RPMBUILD:-"/var/lib/jenkins/rpmbuild"}
-# #RPMBUILD=${RPMBUILD:-"/var/rpmbuild"}
-# RPMBUILD=${RPMBUILD:-"${HOME}/rpmbuild"}
-# 
-# # Clear any old deepsea rpms
-# rm -rf ${RPMBUILD}/RPMS/noarch/deepsea*.rpm
-# 
-# # setup
-# make rpm || {
-#     echo "ERROR: Can't build RPMs"
-#     exit 1
-# }
-# 
-# # copy generated rpms and create a repo
-# 
-# mkdir -p $PUBLISH_DIR
-# cp ${RPMBUILD}/RPMS/noarch/deepsea*.rpm $PUBLISH_DIR
-# createrepo --repo deespea_testing $PUBLISH_DIR
-
-
-# ==============================================================================
-# RUN TEUTHOLOGY TESTS NOW
-
-DEEPSEAREPOURL=$ACCESS_URL
-DEEPSEAREPONAME=deepsea-pr$PULL_ID-b$BUILD_ID
 
 echo DeepSea repo: $DEEPSEAREPOURL
 
@@ -150,7 +110,6 @@ teuthology-openstack -v \
     --ceph-repo ${CEPH_REPO} \
     --ceph ${CEPH_BRANCH} \
     --suite ${TEUTH_SUITE} \
-    --test-repo $DEEPSEAREPONAME:$DEEPSEAREPOURL \
     $TEST_REPO \
     $PWD/deepsea-overrides.yaml \
     --wait 2>&1 | tee $TEUTH_LOG
