3ef84085a89b56d90161a7f3e711a2ae2f93000a
Kyrylo Shatskyy <kyrylo.shatskyy@gmail.com>
2018-09-18 07:51:44 +0200

    Add docker files for openSUSE-15.0

---
+++ docker/deepsea/build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -ex
+TARGETPATH=$(cd ${1:-"$PWD"}; echo $PWD)
+TARGETNAME=${TARGETPATH##*/}
+TARGETBASE=${TARGETNAME,,}
+TARGETBUILD=${TARGETBASE}-rpm
+echo TARGET PATH=$TARGETPATH
+echo TARGET NAME=${TARGETPATH##*/}
+export DHOME=${DHOME:-$(cd $(dirname $BASH_SOURCE); echo $PWD)}
+echo Docker home: $DHOME
+
+[[ -d deepsea ]] ||
+    git clone https://github.com/SUSE/DeepSea.git deepsea
+
+BASEIMAGE=$(grep FROM $TARGETPATH/Dockerfile-base | cut -d ' ' -f 2)
+
+docker pull $BASEIMAGE
+
+docker build \
+	-t $TARGETBASE \
+	-f $TARGETPATH/Dockerfile-base \
+	$TARGETPATH
+
+[[ "$PWD" == "$TARGETPATH" ]] ||
+    cp -a deepsea $TARGETPATH
+
+docker build \
+	--no-cache \
+	-t $TARGETBUILD \
+	-f $TARGETPATH/Dockerfile-rpm \
+	$TARGETPATH
+
+[[ "$PWD" == "$TARGETPATH" ]] ||
+    rm -rf $TARGETPATH/deepsea
+
+docker run -v $(pwd):/mnt $TARGETBUILD:latest sh -c 'mkdir -p /mnt/repo && cp -a rpmbuild/RPMS/* /mnt/repo'
+test -d ./repo
+echo "New deepsea RPMs in ./noarch"
+ls -l ./repo
