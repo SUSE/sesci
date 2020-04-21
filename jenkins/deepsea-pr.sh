a176d397cb7743ef5ea3a166cde29334665accdd
Kyr Shatskyy <kyrylo.shatskyy@gmail.com>
2020-04-21 13:57:48 +0200

    deepsea-pr: move append-artifacts... python code out to a snippet file
    Signed-off-by: Kyr Shatskyy <kyrylo.shatskyy@gmail.com>

--- jenkins/deepsea-pr.sh
+++ jenkins/deepsea-pr.sh
@@ -58,29 +58,8 @@ source $TEUTH_PATH/v/bin/activate
 function teuth_append_artifacts_to_overrides_install_repos() {
     local yaml=${1}
     shift
-    python -c "import sys, yaml
-y = yaml.safe_load(open('$yaml'))
-repos = []
-for i in sys.argv[1:]:
-  artifacts = yaml.safe_load(open(i))['artifacts']
-  for name, a in artifacts.items():
-    url = a['url']
-    if '!' in name:
-        n, p = name.split('!', 1)
-        r = {'name': n, 'priority': int(p), 'url': url}
-    else:
-        r = {'name': name, 'url': url}
-    repos.append(r)
-if 'overrides' not in y:
-    y['overrides'] = {}
-if 'install' not in y['overrides']:
-    y['overrides']['install'] = {}
-if 'repos' not in y['overrides']['install']:
-    y['overrides']['install']['repos'] = []
-y['overrides']['install']['repos'] += repos
-print(yaml.safe_dump(y))
-" $@ | tee temp-overrides.yaml
-cp temp-overrides.yaml $yaml
+    python $SOURCEPATH/snippets/append-artifacts.py $yaml $@
+    cat $yaml
 }
 
 TEUTH_OVERRIDES=overrides-$JOB_NAME-$BUILD_NUMBER.yaml
