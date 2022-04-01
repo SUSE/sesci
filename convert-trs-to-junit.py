42a8d544f7de45bbcc0ff99e7ceeeda5877aed99
Petr Špaček <pspacek@isc.org>
2022-04-01 11:48:12 +0200

    convert-trs-to-junit: fix TabError: inconsistent use of tabs and spaces in indentation
    Mixing tabs and spaces is not permissible in Python 3 anymore.
    Code was converted to space-only indent using vim regex:
    %s/^I/        /g

--- convert-trs-to-junit.py
+++ convert-trs-to-junit.py
@@ -12,10 +12,10 @@ if (len(sys.argv) > 1):
 OutputDirPath = None
 if (len(sys.argv) > 2):
         OutputDirPath = sys.argv[2]
-	try:
-        	os.stat(OutputDirPath)
-	except:
-        	os.mkdir(OutputDirPath)
+        try:
+                os.stat(OutputDirPath)
+        except:
+                os.mkdir(OutputDirPath)
 
 try:
         os.stat(TargetPath)
@@ -29,46 +29,46 @@ passed = 0
 result = []
 
 for root, dirs, files in os.walk(TargetPath):
-	for f in files:
-		if f.endswith(".trs"):
-			p = root + "/" + f
-			#print p
-			amount += 1
-			prefix = root[len(TargetPath) + 1:]
-			output_prefix = "" if OutputDirPath == None else OutputDirPath + "/output/"
-			if prefix != "":
-				prefix += "/"
-			test_name = prefix + f[:-len(".trs")]
+        for f in files:
+                if f.endswith(".trs"):
+                        p = root + "/" + f
+                        #print p
+                        amount += 1
+                        prefix = root[len(TargetPath) + 1:]
+                        output_prefix = "" if OutputDirPath == None else OutputDirPath + "/output/"
+                        if prefix != "":
+                                prefix += "/"
+                        test_name = prefix + f[:-len(".trs")]
 
-			t = {
-				"test": test_name,
-				"log": root + "/" + f[:-len(".trs")] + ".log",
-				"output": output_prefix + test_name.replace("/",".") + ".log"
-			}
+                        t = {
+                                "test": test_name,
+                                "log": root + "/" + f[:-len(".trs")] + ".log",
+                                "output": output_prefix + test_name.replace("/",".") + ".log"
+                        }
 
-			with open(p, "r") as x:
-				d = yaml.load(x)
-				t["result"] = d [':test-result']
-				if d[':test-result'] == 'FAIL':
-					failed +=1
-				else:
-					passed +=1
-			result.append(t)
+                        with open(p, "r") as x:
+                                d = yaml.load(x)
+                                t["result"] = d [':test-result']
+                                if d[':test-result'] == 'FAIL':
+                                        failed +=1
+                                else:
+                                        passed +=1
+                        result.append(t)
 
 test_suites = "ceph"
 test_class = "make-check"
 top = Element('testsuites', { 'name': "ceph" })
 subtop = SubElement( top, 'testsuite', { 'name': 'make-check'})
 for t in result:
-	if OutputDirPath != None:
-		basedir = os.path.dirname(t['output'])
-		try:
-			os.stat(basedir)
-		except:
-			os.makedirs(basedir)
-		print "Copy: " + t['log']
-		print "  to: " + t['output']
-		shutil.copyfile(t['log'], t['output'])
+        if OutputDirPath != None:
+                basedir = os.path.dirname(t['output'])
+                try:
+                        os.stat(basedir)
+                except:
+                        os.makedirs(basedir)
+                print "Copy: " + t['log']
+                print "  to: " + t['output']
+                shutil.copyfile(t['log'], t['output'])
         c  = SubElement(subtop, 'testcase', {
                 'name': t['test'],
                 'classname': test_class,
@@ -84,9 +84,9 @@ from xml.dom import minidom
 
 r = minidom.parseString(ElementTree.tostring(top, 'utf-8'))
 if OutputDirPath == None:
-	print r.toprettyxml(indent="  ")
+        print r.toprettyxml(indent="  ")
 else:
-	with open(OutputDirPath + "/" + test_class + ".xml", 'w') as xml:
-        	xml.write(r.toprettyxml(indent="  "))
-	print str(amount) + " (total) = " + str(passed) + " (passed) + " + str(failed) + " (failed)"
+        with open(OutputDirPath + "/" + test_class + ".xml", 'w') as xml:
+                xml.write(r.toprettyxml(indent="  "))
+        print str(amount) + " (total) = " + str(passed) + " (passed) + " + str(failed) + " (failed)"
 
